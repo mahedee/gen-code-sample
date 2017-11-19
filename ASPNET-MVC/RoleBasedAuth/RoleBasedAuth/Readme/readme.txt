634ad6cb26d4cda9231dab0a3e63a36766608bea
mahedee <mahedee.hasan@gmail.com>
2017-11-19 18:25:33 +0600

    added project - role based authentication

---
+++ ASPNET-MVC/RoleBasedAuth/RoleBasedAuth/Readme/readme.txt
@@ -0,0 +1,7 @@
+﻿Title: Role based authentication using Identity (Individual user login or form based authentication)
+Description:
+
+Tools and Technology:
+1. Visual Studio 2017
+2. ASP.NET MVC
+3. SQL Server
---
+++ ASPNET-MVC/RoleBasedAuth/RoleBasedAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoleBasedAuth.Startup))]
+namespace RoleBasedAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
