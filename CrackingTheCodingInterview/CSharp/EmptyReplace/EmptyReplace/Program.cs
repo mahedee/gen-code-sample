23d0af4d58ddc179c645bd853948061ad0746fc5
mahedee <mahedee.hasan@gmail.com>
2017-12-11 12:49:45 +0600

    added problem 1.4

---
+++ CrackingTheCodingInterview/CSharp/EmptyReplace/EmptyReplace/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyReplace
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Mr. John Smith";
+
+            //Consider size for 5 extra space
+            var characterArray = new char[input.Length + 5 * 2 + 1]; 
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                characterArray[i] = input[i];
+            }
+
+            ReplaceSpaces(characterArray, input.Length);
+            Console.WriteLine("{0} -> {1}", input, new string(characterArray));
+            Console.ReadKey();
+        }
+
+
+
+        static void ReplaceSpaces(char[] input, int length)
+        {
+            int spaceCount = 0;
+
+            // count the number of spaces
+            for (int i = 0; i<length; i++)
+            {
+                if(input[i] == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+
+            // calculate new string size
+            int index = length + spaceCount * 2;
+
+            // copying the characters backwards and inserting % 20
+            for (int i = length-1; i>=0; i--)
+            {
+                if(input[i] == ' ')
+                {
+                    input[index - 1] = '0';
+                    input[index - 2] = '2';
+                    input[index - 3] = '%';
+                    index -= 3;
+                }
+                else
+                {
+                    input[index - 1] = input[i];
+                    index--;
+                }
+            }
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/CSharp/EmptyReplace/EmptyReplace/readme.txt
@@ -0,0 +1,13 @@
+﻿Language: Visual C#
+IDE: Visual Studio 2015
+
+Source 
+Book : Cracking the coding interview - 5th Edition
+Problem : 1.4
+
+Question: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end of the string 
+to hold the additional characters, and that you are given the "true" length of the string. (Note: if implement in c#, please use a character array 
+so that you can perform this operation in place. Please don't use string.Replace()) 
+Sample input: "Mr. Kamal Hossain"
+Sample Output: "Mr.%20Kamal%20Hossain"
+Hints : NA
