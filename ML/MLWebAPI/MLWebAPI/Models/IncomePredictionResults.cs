a5278e9c3e6893782b92cfa498c8413f80ebfdc2
mahedee <mahedee.hasan@gmail.com>
2018-01-23 11:49:06 +0600

    added api project

---
+++ ML/MLWebAPI/MLWebAPI/Models/IncomePredictionResults.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MLWebAPI.Models
+{
+    public class IncomePredictionResults
+    {
+        public string Age { get; set; }
+        public string WorkClass { get; set; }
+        public string FnlWgt { get; set; }
+
+        public string Education { get; set; }
+        public string EducationNum { get; set; }
+        public string MaritalStatus { get; set; }
+
+        public string Occupation { get; set; }
+        public string Relationship { get; set; }
+        public string Race { get; set; }
+
+        public string Sex { get; set; }
+        public string CapitalGain { get; set; }
+        public string CapitalLoss { get; set; }
+
+        public string HoursPerWeek { get; set; }
+        public string NativeCountry { get; set; }
+        public string MLPrediction { get; set; }
+        public string MLConfidence { get; set; }
+        public string MLResponseTime { get; set; }
+    }
+}
